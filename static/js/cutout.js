5c0f1eee5634da8db35ed7be3ae3fe51712501df
Akihi <2579500994@qq.com>
2024-11-05 10:06:55 +0800

    Modify the path to point to netlify

--- static/js/cutout.js
+++ static/js/cutout.js
@@ -13,24 +13,24 @@ document.getElementById('processButton').addEventListener('click', async () => {
     const formData = new FormData();
     formData.append('image_file', uploadedFile);
 
-try {
-    const response = await fetch('/api/rembg', {
-        method: 'POST',
-        body: formData,
-    });
+    try {
+        const response = await fetch('/.netlify/functions/rembg', {
+            method: 'POST',
+            body: formData,
+        });
 
-    if (!response.ok) {
-        const errorMessage = await response.text(); // 捕获错误信息
-        throw new Error(`抠图失败: ${errorMessage}`);
+        if (!response.ok) {
+            const errorMessage = await response.text(); 
+            throw new Error(`抠图失败: ${errorMessage}`);
+        }
+
+        const imgBlob = await response.blob();
+        const imgURL = URL.createObjectURL(imgBlob);
+        const resultImage = document.getElementById('resultImage');
+        resultImage.src = imgURL;
+        resultImage.style.display = 'block';
+    } catch (error) {
+        console.error('抠图失败:', error);
+        alert(`抠图失败，请重试！错误信息: ${error.message}`);
     }
-
-    const imgBlob = await response.blob();
-    const imgURL = URL.createObjectURL(imgBlob);
-    const resultImage = document.getElementById('resultImage');
-    resultImage.src = imgURL;
-    resultImage.style.display = 'block';
-} catch (error) {
-    console.error('抠图失败:', error);
-    alert(`抠图失败，请重试！错误信息: ${error.message}`);
-}
 });
